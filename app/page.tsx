55c94b35af143d2c4d81b814e9c5b6ec979fbc2e
Aleklz90 <novogogosha@gmail.com>
2026-08-19 20:56:05 +0300

    feat: add input form validation

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,219 @@
+"use client";
+
+import { useState } from "react";
 import { FcHome } from "react-icons/fc";
 
+type inputData = {
+  annualRent: number;
+  cheques: number;
+  propertyType: string;
+  furnished: boolean;
+  chiller: boolean;
+  firstMove: boolean;
+};
+
+const iniErrorObj = {
+  annualRent: "",
+  cheques: "",
+  propertyType: "",
+  furnished: "",
+  chiller: "",
+  firstMove: "",
+};
+
+const initData = {
+  annualRent: 0,
+  cheques: 0,
+  propertyType: "",
+  furnished: false,
+  chiller: false,
+  firstMove: false,
+};
+
+const initTouchedFields = {
+  annualRent: false,
+  cheques: false,
+  propertyType: false,
+  furnished: false,
+  chiller: false,
+  firstMove: false,
+};
+
 export default function Home() {
+  const [error, setError] = useState(iniErrorObj);
+  const [formData, setFormData] = useState(initData);
+  const [touchedFields, setTouchedFields] = useState(initTouchedFields);
+
+  const checkData = (data: inputData) => {
+    console.log(data);
+    let updatedErrorObj = { ...error };
+    let updatedFormObj = { ...formData };
+    if (touchedFields.annualRent) {
+      if (isNaN(data.annualRent)) {
+        updatedErrorObj = {
+          ...updatedErrorObj,
+          annualRent: "annual rent should be a number",
+        };
+      } else if (data.annualRent <= 0) {
+        updatedErrorObj = {
+          ...updatedErrorObj,
+          annualRent: "annual rent should be greater than 0",
+        };
+      } else {
+        updatedErrorObj = {
+          ...updatedErrorObj,
+          annualRent: "",
+        };
+        updatedFormObj = { ...updatedFormObj, annualRent: data.annualRent };
+      }
+    }
+
+    if (touchedFields.cheques) {
+      if (data.cheques) {
+        updatedFormObj = { ...updatedFormObj, cheques: data.cheques };
+        updatedErrorObj = {
+          ...updatedErrorObj,
+          cheques: "",
+        };
+      } else {
+        updatedErrorObj = {
+          ...updatedErrorObj,
+          cheques: "You have to choose cheques number",
+        };
+      }
+    }
+
+    if (touchedFields.propertyType) {
+      if (data.propertyType) {
+        updatedFormObj = { ...updatedFormObj, propertyType: data.propertyType };
+        updatedErrorObj = {
+          ...updatedErrorObj,
+          propertyType: "",
+        };
+      } else {
+        updatedErrorObj = {
+          ...updatedErrorObj,
+          propertyType: "You have to choose property type",
+        };
+      }
+    }
+
+    updatedFormObj = {
+      ...updatedFormObj,
+      furnished: data.furnished,
+      chiller: data.chiller,
+      firstMove: data.firstMove,
+    };
+    updatedErrorObj = {
+      ...updatedErrorObj,
+      furnished: "",
+    };
+
+    setFormData(updatedFormObj);
+
+    setError(updatedErrorObj);
+  };
+
+  const buttonCheck = () => {
+    console.log(formData.annualRent, formData.cheques, formData.propertyType);
+    if (formData.annualRent && formData.cheques && formData.propertyType) {
+      return false;
+    } else {
+      return true;
+    }
+  };
   return (
     <div className="flex gap-15">
       <div>
         <form action="">
           <div>
             <label htmlFor="annual_rent">Annual rent</label>
-            <input type="text" id="annual_rent" />
+            <input
+              type="text"
+              id="annual_rent"
+              onClick={() =>
+                setTouchedFields({ ...touchedFields, annualRent: true })
+              }
+              onBlurCapture={(e) =>
+                checkData({ ...formData, annualRent: Number(e.target.value) })
+              }
+            />
+            <div className="error">{error.annualRent}</div>
           </div>
           <div>
-            <label htmlFor="cheques">Cheques</label>
-            <input type="text" id="cheques" />
+            <div className="flex flex-col">
+              <label htmlFor="cheques">Cheques</label>
+              <select
+                id="cheques"
+                onClick={() =>
+                  setTouchedFields({ ...touchedFields, cheques: true })
+                }
+                onBlurCapture={(e) =>
+                  checkData({ ...formData, cheques: Number(e.target.value) })
+                }
+              >
+                <option value="">Select number...</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="4">4</option>
+                <option value="6">6</option>
+                <option value="12">12</option>
+              </select>
+            </div>
+            <div className="error">{error.cheques}</div>
           </div>
           <div>
             <label htmlFor="property_type">Property type</label>
-            <input type="text" id="property_type" />
+            <select
+              id="property_type"
+              onClick={() =>
+                setTouchedFields({ ...touchedFields, propertyType: true })
+              }
+              onBlurCapture={(e) =>
+                checkData({ ...formData, propertyType: e.target.value })
+              }
+            >
+              <option value="">Select number...</option>
+              <option value="apartment">apartment</option>
+              <option value="villa">villa</option>
+            </select>
+            <div className="error">{error.propertyType}</div>
           </div>
-          <div>
+          <div className="flex gap-2 flex-col">
             <label htmlFor="furnished">Furnished</label>
-            <input type="text" id="furnished" />
+            <input
+              type="checkbox"
+              id="furnished"
+              onClick={() => {
+                setTouchedFields({ ...touchedFields, furnished: true });
+                checkData({ ...formData, furnished: !formData.furnished });
+              }}
+            />
+            <div className="error">{error.furnished}</div>
           </div>
-          <div>
+          <div className="flex gap-2 flex-col">
             <label htmlFor="chiller">Chiller included in rent</label>
-            <input type="text" id="chiller" />
+            <input
+              type="checkbox"
+              id="chiller"
+              onClick={() => {
+                checkData({ ...formData, chiller: !formData.chiller });
+              }}
+            />
+            <div className="error">{error.chiller}</div>
           </div>
-          <div>
+          <div className="flex gap-2 flex-col">
             <label htmlFor="first_move">First move in Dubai</label>
-            <input type="text" id="first_move" />
+            <input
+              type="checkbox"
+              id="first_move"
+              onClick={() => {
+                checkData({ ...formData, firstMove: !formData.firstMove });
+              }}
+            />
+            <div className="error">{error.firstMove}</div>
           </div>
-          <button>Calculate</button>
+          <button disabled={buttonCheck()}>Calculate</button>
         </form>
       </div>
       <div>
